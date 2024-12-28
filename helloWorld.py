3b2c2a882675f81575c94af8af2ccaa9661e3640
Kevin Greaney <kevin.greaney05@gmail.com>
2024-12-28 20:02:58 +0000

    Adding helloWorld.py to byb_project repository

---
+++ helloWorld.py
@@ -0,0 +1,14 @@
+
+
+
+# ================Pseudo Code for Auto_graded task 1================
+# 1. copy task into python and comment out all the task, for ease of reference
+# 2. write the code to ask the user for their name , save as a variable 
+username = input("Please enter your name:")
+# 3. print the output of the variable name
+print (username)
+# 4. repeat steps 2 and 3 for the users age.
+userage = input("Please enter your age:")
+print (username + ",you are " + userage + " years old.")
+# 5. Print Hello World  on a new line
+print ("Hello World!")
